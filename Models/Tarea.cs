c785fe4c38a53610ec0e557e896090b312977aa6
Federico <federicoaacevedo@hotmail.com>
2023-05-07 02:38:42 -0300

    Models

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,15 +1,25 @@
-﻿namespace proyectoef.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proyectoef.Models
 {
     public class Tarea
     {
+        //[Key]
         public Guid TareaId { get; set; }
+        //[ForeignKey("CategoriaId")]
         public Guid CategoriaId { get; set; }
+        //[Required]
+        //[MaxLength(255)]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
 
         public Prioridad PrioridadTarea { get; set; }
         public DateTime FechaCreacion { get; set; }
         public virtual Categoria Categoria { get; set; }
+
+        [NotMapped]
+        public string Resumen { get; set; }
         
     }
 
--- TareasContext.cs
+++ TareasContext.cs
@@ -9,5 +9,30 @@ namespace proyectoef
         public DbSet<Tarea> Tareas { get; set; }
 
         public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>(categoria =>
+            {
+                categoria.ToTable("Categoria");
+                categoria.HasKey(p => p.CategoriaId);
+
+                categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
+
+                categoria.Property(p => p.Descripcion);
+            });
+
+            modelBuilder.Entity<Tarea>(tarea => {
+
+                tarea.ToTable("Tarea");
+                tarea.HasKey(t => t.TareaId);
+                tarea.Property(t => t.CategoriaId);
+                tarea.Property(t => t.Titulo).IsRequired().HasMaxLength(200);
+                tarea.Property(t => t.Descripcion).HasMaxLength(500);
+                tarea.Property(t => t.PrioridadTarea).IsRequired();
+                tarea.Property(t => t.FechaCreacion);
+
+            });
+        }
     }
 }
